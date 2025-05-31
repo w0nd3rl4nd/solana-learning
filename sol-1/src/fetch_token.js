052136e1e14c81821a8c2109f38bb003161113a0
w0nd3rl4nd <alicealiagamoreno@gmail.com>
2025-05-31 23:05:25 +0200

    Create fetch_token

---
+++ sol-1/src/fetch_token.js
@@ -0,0 +1,25 @@
+import { Connection, PublicKey } from "@solana/web3.js";
+
+const connection = new Connection(
+  "https://api.mainnet-beta.solana.com",
+  "confirmed"
+);
+const address = new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA");
+const accountInfo = await connection.getAccountInfo(address);
+
+console.log(
+  JSON.stringify(
+    accountInfo,
+    (key, value) => {
+      if (key === "data" && value && value.length > 1) {
+        return [
+          value[0],
+          "...truncated, total bytes: " + value.length + "...",
+          value[value.length - 1]
+        ];
+      }
+      return value;
+    },
+    2
+  )
+);
