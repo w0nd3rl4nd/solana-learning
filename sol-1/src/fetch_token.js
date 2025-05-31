c99765b46195afe32e0e5e3a34ef796ddc0ff14d
w0nd3rl4nd <alicealiagamoreno@gmail.com>
2025-05-31 23:07:07 +0200

    Comment fetch_token

--- sol-1/src/fetch_token.js
+++ sol-1/src/fetch_token.js
@@ -1,12 +1,17 @@
+// This time we only import PublicKey as we don't need to generate a keypair
 import { Connection, PublicKey } from "@solana/web3.js";
 
+// We create a connection to the Solana mainnet
 const connection = new Connection(
   "https://api.mainnet-beta.solana.com",
   "confirmed"
 );
+
+// We define the address as a known one and create an object with it's information
 const address = new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA");
 const accountInfo = await connection.getAccountInfo(address);
 
+// We use stringify to truncate the very long data portion that is in byte code
 console.log(
   JSON.stringify(
     accountInfo,
