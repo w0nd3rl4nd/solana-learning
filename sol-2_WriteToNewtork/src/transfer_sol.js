33ebf9f46785f7f3f7cb0d16fb1ed31dd6748fb7
w0nd3rl4nd <alicealiagamoreno@gmail.com>
2025-06-01 09:52:17 +0200

    Create transfer_sol

---
+++ sol-2_WriteToNewtork/src/transfer_sol.js
@@ -0,0 +1,39 @@
+import {
+    LAMPORTS_PER_SOL,
+    SystemProgram,
+    Transaction,
+    sendAndConfirmTransaction,
+    Keypair,
+    Connection
+} from "@solana/web3.js";
+
+const connection = new Connection("http://localhost:8899", "confirmed");
+
+const sender = new Keypair();
+const receiver = new Keypair();
+
+const signature = await connection.requestAirdrop(
+    sender.publicKey,
+    LAMPORTS_PER_SOL  * 5
+)
+await connection.confirmTransaction(signature, "confirmed");
+
+const transferInstruction = SystemProgram.transfer({
+    fromPubkey: sender.publicKey,
+    toPubkey: receiver.publicKey,
+    lamports: 0.01 * LAMPORTS_PER_SOL
+});
+
+const transaction = new Transaction().add(transferInstruction);
+
+const transactionSignature = await sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [sender]
+);
+
+console.log(`Transaction signature: ${transactionSignature}`)
+const receiverAccountInfo = await connection.getAccountInfo(receiver.publicKey);
+const senderAccountInfo = await connection.getAccountInfo(sender.publicKey);
+console.log(JSON.stringify(senderAccountInfo, null, 2));
+console.log(JSON.stringify(receiverAccountInfo, null, 2));
